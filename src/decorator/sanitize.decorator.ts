8c3e1d0d3068aa6995af516c8006cfdfd6edad84
Elyas Erfani <elyaserfani2@gmail.com>
2023-05-10 15:11:15 +0430

    refactor: :recycle: Refactor sanitizer decorator
    Refactor sanitizer decorator

--- src/decorator/sanitize.decorator.ts
+++ src/decorator/sanitize.decorator.ts
@@ -1,26 +1,14 @@
 import { Transform, TransformOptions } from 'class-transformer';
 import * as sanitizeHtml from 'sanitize-html';
 
-export interface EscapeOptions {
-  ignoreFormattingTags?: boolean;
-}
-
 export function Sanitize(
-  options?: EscapeOptions,
   transformOptions?: TransformOptions,
 ): (target: object, key: string) => void {
   return Transform(({ value }) => {
     if ('string' !== typeof value) {
       return value;
     }
-    let text = null;
-    if (options?.ignoreFormattingTags) {
-      text = sanitizeHtml(value);
-    } else {
-      text = sanitizeHtml(value, {
-        allowedTags: [],
-      });
-    }
+    const text = sanitizeHtml(value);
     return text;
   }, transformOptions);
 }
