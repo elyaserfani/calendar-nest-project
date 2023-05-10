c6325a0557e754c93d4a3f4b954c8a7f170a743c
Elyas Erfani <elyaserfani2@gmail.com>
2023-05-10 15:15:25 +0430

    perf: :sparkles: Update decorator index exports
    Update decorator index exports

--- src/decorator/index.ts
+++ src/decorator/index.ts
@@ -1,3 +1,5 @@
 export * from './sanitize.decorator';
 export * from './trim.decorator';
 export * from './swagger.custom.exception.decorator';
+export * from './auth.decorator';
+export * from './no.guard.decorator';
