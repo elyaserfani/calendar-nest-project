10baf1bad1efc4b9cd4148626dfb4fc4b993fcc2
Elyas Erfani <elyaserfani2@gmail.com>
2023-05-14 09:57:29 +0430

    feat: :art: Add user.service login unit tests
    Add user.service login unit tests

--- src/module/user/user.service.spec.ts
+++ src/module/user/user.service.spec.ts
@@ -3,20 +3,29 @@ import { getRepositoryToken } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Role, User } from '../../entity';
 import { JwtHelper } from '../../helper/jwt.helper';
-import { RegisterRequestDto } from './dto';
+import { LoginRequestDto, RegisterRequestDto } from './dto';
 import { UserService } from './user.service';
+import * as bcrypt from 'bcrypt';
+import { DateHelper } from '../../helper';
+import { BadRequestException, NotFoundException } from '../../exception';
+import { JwtService } from '@nestjs/jwt';
+import * as dotenv from 'dotenv';
 
 describe('UserService', () => {
-  let service: UserService;
+  let userService: UserService;
   let userRepository: Repository<User>;
   let roleRepository: Repository<Role>;
   let jwtHelper: JwtHelper;
+  let dateHelper: DateHelper;
 
   beforeEach(async () => {
+    dotenv.config();
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         UserService,
+        JwtService,
         JwtHelper,
+        DateHelper,
         {
           provide: getRepositoryToken(User),
           useClass: Repository,
@@ -28,10 +37,11 @@ describe('UserService', () => {
       ],
     }).compile();
 
-    service = module.get<UserService>(UserService);
+    userService = module.get<UserService>(UserService);
     userRepository = module.get<Repository<User>>(getRepositoryToken(User));
     roleRepository = module.get<Repository<Role>>(getRepositoryToken(Role));
     jwtHelper = module.get<JwtHelper>(JwtHelper);
+    dateHelper = module.get<DateHelper>(DateHelper);
   });
 
   afterEach(() => {
@@ -39,25 +49,6 @@ describe('UserService', () => {
   });
 
   describe('registerUser', () => {
-    const mockUser: User = {
-      id: 1,
-      username: 'testuser',
-      email: 'testuser@example.com',
-      password: 'password',
-      nickname: 'Test User',
-      role: 'ROLE_USER',
-      created_at: new Date(),
-      updated_at: new Date(),
-      events: null,
-    };
-    const mockRegisterRequestDto: RegisterRequestDto = {
-      username: 'testuser',
-      email: 'testuser@example.com',
-      password: 'password',
-      nickname: 'Test User',
-      role: 'user',
-    };
-
     it('should register a new user', async () => {});
 
     it('should throw a BadRequestException if user already exists', async () => {});
@@ -66,22 +57,60 @@ describe('UserService', () => {
   });
 
   describe('loginUser', () => {
-    const mockUser: User = {
-      id: 1,
-      username: 'testuser',
-      email: 'testuser@example.com',
-      password: 'password',
-      nickname: 'Test User',
-      role: 'ROLE_USER',
-      created_at: new Date(),
-      updated_at: new Date(),
-      events: null,
-    };
+    it('should login user with valid credentials', async () => {
+      const user = new User();
+      user.id = 1;
+      user.username = 'testuser';
+      user.email = 'testuser@example.com';
+      user.nickname = 'Test User';
+      user.password = await bcrypt.hash('password', 10);
+      user.role = 'ROLE_USER';
 
-    it('should login user', async () => {});
+      jest.spyOn(userRepository, 'findOneBy').mockResolvedValue(user);
 
-    it('should throw a BadRequestException if credentials is invalid', async () => {});
+      const loginRequestDto = new LoginRequestDto();
+      loginRequestDto.username = 'testuser';
+      loginRequestDto.password = 'password';
+      const result = await userService.loginUser(loginRequestDto);
 
-    it('should throw a NotFoundException if user with this username not found', async () => {});
+      expect(result).toEqual({
+        data: {
+          user: {
+            id: user.id,
+            username: user.username,
+            email: user.email,
+            nickname: user.nickname,
+            role: user.role,
+            accessToken: expect.any(String),
+          },
+        },
+      });
+    });
+    it('should throw a NotFoundException when provided with an invalid username', async () => {
+      jest.spyOn(userRepository, 'findOneBy').mockResolvedValue(undefined);
+      const loginRequestDto = new LoginRequestDto();
+      loginRequestDto.username = 'invaliduser';
+      loginRequestDto.password = 'password';
+      await expect(userService.loginUser(loginRequestDto)).rejects.toThrow(
+        NotFoundException,
+      );
+    });
+
+    it('should throw a BadRequestException when provided with invalid credentials', async () => {
+      const user = new User();
+      user.id = 1;
+      user.username = 'testuser';
+      user.email = 'testuser@example.com';
+      user.nickname = 'Test User';
+      user.password = await bcrypt.hash('password', 10);
+      user.role = 'ROLE_USER';
+      jest.spyOn(userRepository, 'findOneBy').mockResolvedValue(user);
+      const loginRequestDto = new LoginRequestDto();
+      loginRequestDto.username = 'testuser';
+      loginRequestDto.password = 'invalidpassword';
+      await expect(userService.loginUser(loginRequestDto)).rejects.toThrow(
+        BadRequestException,
+      );
+    });
   });
 });
