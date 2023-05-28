056f5252d4636a9cbec189602db8f2a31fc689f7
Elyas Erfani <elyaserfani2@gmail.com>
2023-05-28 12:14:54 +0430

    feat: :sparkles: Create roles and permissions decorator
    Create roles and permissions decorator

--- src/decorators/index.ts
+++ src/decorators/index.ts
@@ -3,3 +3,5 @@ export * from './trim.decorator';
 export * from './swagger.custom.exception.decorator';
 export * from './auth.decorator';
 export * from './no.guard.decorator';
+export * from './roles.decorator';
+export * from './permissions.decorator';
