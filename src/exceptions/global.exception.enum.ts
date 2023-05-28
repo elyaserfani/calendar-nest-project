4266c41a172144e4d9fe2eae86963f006eae4c39
Elyas Erfani <elyaserfani2@gmail.com>
2023-05-28 10:35:29 +0430

    feat: :art: Update jwt auth guard for role checking
    Update jwt auth guard for role checking

--- src/exceptions/global.exception.enum.ts
+++ src/exceptions/global.exception.enum.ts
@@ -1,5 +1,6 @@
 export enum GlobalExceptionEnum {
   UNAUTHORIZED = 'Unauthorized Error',
+  PERMISSION_ERROR = 'You Do Not Have Permission To Access This Resource',
   VALIDATION_ERROR = 'Validation Error',
   INTERNAL_SERVER_ERROR = 'Internal Server Error',
 }
--- src/guards/jwt.auth.guard.ts
+++ src/guards/jwt.auth.guard.ts
@@ -7,10 +7,15 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
   canActivate(context: ExecutionContext) {
     return super.canActivate(context);
   }
-  handleRequest(err: any, user: any, info: any) {
+  handleRequest(err: any, user: any, context: any) {
     if (err || !user) {
       throw err || new UnauthorizedException(GlobalExceptionEnum.UNAUTHORIZED);
+    } else {
+      const requiredRole = context.getHandler().role;
+      if (!user || (requiredRole && !user.roles.includes(requiredRole))) {
+        throw new UnauthorizedException(GlobalExceptionEnum.PERMISSION_ERROR);
+      }
+      return user;
     }
-    return user;
   }
 }
